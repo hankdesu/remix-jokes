ea916b4a616b915a5359b5dd6ecff68aa76c2dfd
Hank <hankchou@climax.com.tw>
2021-12-23 10:31:18 +0000

    feat(*): first commit

---
+++ app/routes/jokes/new.tsx
@@ -0,0 +1,46 @@
+import type { ActionFunction } from "remix";
+import { redirect } from "remix";
+import { db } from "~/utils/db.server";
+
+export const action: ActionFunction = async ({ request }) => {
+    const form = await request.formData();
+    const name = form.get("name");
+    const content = form.get("content");
+
+    if (
+        typeof name !== "string" ||
+        typeof content !== "string"
+    ) {
+        throw new Error(`Form not submitted correctly.`);
+    }
+
+    const fields = { name, content };
+
+    const joke = await db.joke.create({ data: fields });
+    return redirect(`/jokes/${joke.id}`);
+}
+
+export default function NewJokeRoute() {
+    return (
+        <div>
+            <p>Add your own hilarious joke</p>
+            <form method="post">
+                <div>
+                    <label>
+                        Name: <input type="text" name="name" />
+                    </label>
+                </div>
+                <div>
+                    <label>
+                        Content: <textarea name="content" />
+                    </label>
+                </div>
+                <div>
+                    <button type="submit" className="button">
+                        Add
+                    </button>
+                </div>
+            </form>
+        </div>
+    );
+}
